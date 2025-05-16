407281edb0594e34399ca0d5a95efafa09153d41
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-16 10:59:54 +0900

    feat(ping): add ping.go

---
+++ internal/root/ping.go
@@ -0,0 +1,12 @@
+package root
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func PingHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/plain")
+    w.WriteHeader(http.StatusOK)
+    fmt.Fprint(w, "pong")
+}
