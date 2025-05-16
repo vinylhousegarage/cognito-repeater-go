5a90f4ed856931843a13e9a0d43d207a3b3f3b11
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-16 11:43:13 +0900

    test(ping): create test/root; add ping route test

---
+++ test/root/ping_route_test.go
@@ -0,0 +1,26 @@
+package router_test
+
+import (
+    "net/http"
+    "net/http/httptest"
+    "testing"
+    "io"
+
+    "cognito-repeater-go/internal/router"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestRouter_PingRouteRegistered(t *testing.T) {
+    r := router.NewRouter()
+
+    req := httptest.NewRequest(http.MethodGet, "/ping", nil)
+    w := httptest.NewRecorder()
+
+    r.ServeHTTP(w, req)
+
+    resp := w.Result()
+    body, _ := io.ReadAll(resp.Body)
+
+    assert.Equal(t, http.StatusOK, resp.StatusCode)
+    assert.Equal(t, "pong", string(body))
+}
