de514bcb826cdfc3197ee35d956a7122c3048acc
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-17 18:45:56 +0900

    test(service): update service_test.go to test fetchMetadata()

--- internal/auth/service_test.go
+++ internal/auth/service_test.go
@@ -1,13 +1,34 @@
 package auth
 
 import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParseCognitoLogoutURL_ReturnExpected(t *testing.T) {
-	expected := "https://stab.com/logout"
-	actual := parseCognitoLogoutURL()
-	assert.Equal(t, expected, actual)
+type Metadata struct {
+	EndSessionEndpoint string `json:"end_session_endpoint"`
+}
+
+func Test_fetchMetadata_Success(t *testing.T) {
+	mockMetadata := Metadata{
+		EndSessionEndpoint: "https://mock.example.com/logout",
+	}
+
+	mockJSON, err := json.Marshal(mockMetadata)
+	assert.NoError(t, err)
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write(mockJSON)
+	}))
+	defer ts.Close()
+
+	result, err := fetchMetadata(ts.URL)
+
+	assert.NoError(t, err)
+	assert.Equal(t, mockMetadata.EndSessionEndpoint, result.EndSessionEndpoint)
 }
