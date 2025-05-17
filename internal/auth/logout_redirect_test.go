e9a6376201b69915445ee4e65ab9e63ffe6bb6df
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-17 11:35:16 +0900

    test(logout_redirect): fix typo (Logout Successful -> Logout successful)

--- internal/auth/logout_redirect_test.go
+++ internal/auth/logout_redirect_test.go
@@ -25,7 +25,7 @@ func TestMetadataHandler_ReturnsExpectedStatusAndJSONBody(t *testing.T) {
 	assert.NoError(t, err)
 
 	expected := map[string]string{
-		"message": "Logout Successful",
+		"message": "Logout successful",
 	}
 
 	var actual map[string]string
