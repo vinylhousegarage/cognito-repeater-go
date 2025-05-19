5b7f394b6c0f099893f9a9d42360802c68769d5c
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-19 18:19:01 +0900

    refactor(auth): remove LogoutHandler from routes.go due to logout redesign

--- internal/auth/routes.go
+++ internal/auth/routes.go
@@ -6,6 +6,5 @@ import (
 )
 
 func RegisterAuthRoutes(mux *http.ServeMux, p config.MetadataURLProvider) {
-	mux.HandleFunc("/logout", LogoutHandler(p))
 	mux.HandleFunc("/logout/redirect", LogoutRedirectHandler)
 }
