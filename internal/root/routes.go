d203a0296fd8d2922b05b22b23ab00644ce5195d
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-19 20:09:18 +0900

    fix(root): import ping package to resolve routing error

--- internal/root/routes.go
+++ internal/root/routes.go
@@ -1,6 +1,10 @@
 package root
 
-import "net/http"
+import (
+	"net/http"
+
+	"cognito-repeater-go/internal/root/ping"
+)
 
 func RegisterRootRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("/ping", PingHandler)
