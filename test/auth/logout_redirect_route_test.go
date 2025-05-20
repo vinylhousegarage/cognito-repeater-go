5670321f348e51029ff97a7d9e756d0cd8e16df9
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-20 09:12:44 +0900

    fix(test): add initial fixtures.go

--- test/auth/logout_redirect_route_test.go
+++ test/auth/logout_redirect_route_test.go
@@ -13,16 +13,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var mockCfg = &config.Config{
-	Region:           "ap-northeast-1",
-	ClientSecret:     "client-secret",
-	LogoutURI:        "https://example.com/logout",
-	RedirectURI:      "https://localhost/callback",
-	Scope:            "openid",
-	UserPoolClientID: "client-id",
-	UserPoolID:       "pool-id",
-}
-
 func TestRouter_LogoutRedirectRoute_ReturnsExpectedJSON(t *testing.T) {
 	t.Parallel()
 
---
+++ test/fixtures.go
@@ -0,0 +1,11 @@
+package test
+
+var mockCfg = &config.Config{
+	Region:           "ap-northeast-1",
+	ClientSecret:     "client-secret",
+	LogoutURI:        "https://example.com/logout",
+	RedirectURI:      "https://localhost/callback",
+	Scope:            "openid",
+	UserPoolClientID: "client-id",
+	UserPoolID:       "pool-id",
+}
