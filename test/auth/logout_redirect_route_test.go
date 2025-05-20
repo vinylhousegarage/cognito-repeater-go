144bfc256e1472fb87f5b0a425197a27da58347f
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-20 10:57:46 +0900

    fix(test): import test_helpers, replace mockCfg with MockCfg, and rename package to auth_test

--- test/auth/logout_redirect_route_test.go
+++ test/auth/logout_redirect_route_test.go
@@ -1,4 +1,4 @@
-package test
+package auth_test
 
 import (
 	"encoding/json"
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"cognito-repeater-go/internal/router"
+	"cognito-repeater-go/internal/test/test_helpers"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -15,7 +16,7 @@ import (
 func TestRouter_LogoutRedirectRoute_ReturnsExpectedJSON(t *testing.T) {
 	t.Parallel()
 
-	r := router.NewRouter(mockCfg)
+	r := router.NewRouter(test_helpers.MockCfg)
 
 	req := httptest.NewRequest(http.MethodGet, "/logout/redirect", nil)
 	w := httptest.NewRecorder()
