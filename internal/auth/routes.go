4e69cb47bc0d8cc4da69c3b2e740e8ceec2fdd85
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-20 13:54:42 +0900

    feat(logout): register LogoutHandler

--- internal/auth/routes.go
+++ internal/auth/routes.go
@@ -3,9 +3,11 @@ package auth
 import (
 	"net/http"
 
+	"cognito-repeater-go/internal/auth/logout"
 	"cognito-repeater-go/internal/auth/logoutredirect"
 )
 
-func RegisterAuthRoutes(mux *http.ServeMux) {
+func RegisterAuthRoutes(mux *http.ServeMux, p logout.LogoutEndpointProvider) {
+	mux.HandleFunc("/logout", logout.LogoutHandler(p))
 	mux.HandleFunc("/logout/redirect", logoutredirect.LogoutRedirectHandler)
 }
