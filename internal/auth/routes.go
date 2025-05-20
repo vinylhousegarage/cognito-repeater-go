d17a68a4056715efbdaa1d4533ad8401ea686d0f
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-20 14:35:50 +0900

    fix(auth): add config.MetadataURLProvider argument to LogoutHandler

--- internal/auth/routes.go
+++ internal/auth/routes.go
@@ -5,9 +5,10 @@ import (
 
 	"cognito-repeater-go/internal/auth/logout"
 	"cognito-repeater-go/internal/auth/logoutredirect"
+	"cognito-repeater-go/internal/config"
 )
 
-func RegisterAuthRoutes(mux *http.ServeMux, p logout.LogoutEndpointProvider) {
-	mux.HandleFunc("/logout", logout.LogoutHandler(p))
+func RegisterAuthRoutes(mux *http.ServeMux, provider logout.LogoutEndpointProvider, p config.MetadataURLProvider) {
+	mux.HandleFunc("/logout", logout.LogoutHandler(provider, p))
 	mux.HandleFunc("/logout/redirect", logoutredirect.LogoutRedirectHandler)
 }
