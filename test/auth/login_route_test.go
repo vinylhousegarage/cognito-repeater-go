14c547e800b9c1fb2138056acfdfa754d1598698
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-22 07:16:45 +0900

    test(login): add login_route_test.go

---
+++ test/auth/login_route_test.go
@@ -0,0 +1,26 @@
+package auth_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"cognito-repeater-go/internal/router"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoginRouteIsRegisteredInProductionRouter(t *testing.T) {
+	t.Parallel()
+
+	router := router.NewRouter()
+
+	req := httptest.NewRequest(http.MethodGet, "/login", nil)
+	w := httptest.NewRecorder()
+
+	router.ServeHTTP(w, req)
+
+	resp := w.Result()
+	assert.Equal(t, http.StatusFound, resp.StatusCode)
+	assert.Contains(t, resp.Header.Get("Location"), "oauth2/authorize")
+}
