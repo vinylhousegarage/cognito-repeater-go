4a4eb54f589173d377db2684ea15e25f6c4fd83a
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-22 07:25:30 +0900

    feat(login): add LoginClient to AuthHandler; add LoginHandler to RegisterAuthRoutes

--- internal/auth/handlers.go
+++ internal/auth/handlers.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"cognito-repeater-go/internal/auth/login"
 	"cognito-repeater-go/internal/auth/logout"
 	"cognito-repeater-go/internal/config"
 	"net/http"
@@ -8,6 +9,7 @@ import (
 
 func NewAuthHandlers(cfg config.MetadataURLProvider, client *http.Client) AuthHandlers {
 	return AuthHandlers{
+		LoginURLProvider:    login.NewLoginClient(client),
 		LogoutURLProvider:   logout.NewLogoutClient(client),
 		MetadataURLProvider: cfg,
 	}
--- internal/auth/routes.go
+++ internal/auth/routes.go
@@ -3,17 +3,20 @@ package auth
 import (
 	"net/http"
 
+	"cognito-repeater-go/internal/auth/login"
 	"cognito-repeater-go/internal/auth/logout"
 	"cognito-repeater-go/internal/auth/logoutredirect"
 	"cognito-repeater-go/internal/config"
 )
 
 type AuthHandlers struct {
+	LoginURLProvider    login.LoginURLProvider
 	LogoutURLProvider   logout.LogoutURLProvider
 	MetadataURLProvider config.MetadataURLProvider
 }
 
 func RegisterAuthRoutes(mux *http.ServeMux, h AuthHandlers) {
+	mux.HandleFunc("/login", login.LoginHandler(h.LoginURLProvider, h.MetadataURLProvider))
 	mux.HandleFunc("/logout", logout.LogoutHandler(h.LogoutURLProvider, h.MetadataURLProvider))
 	mux.HandleFunc("/logout/redirect", logoutredirect.LogoutRedirectHandler)
 }
