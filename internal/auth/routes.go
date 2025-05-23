e5fc6dc1b959bb8e342efe30c6c633bda6f8a895
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-24 08:25:29 +0900

    refactor(routes): replace AuthHandlers with HandlerDependencies

--- internal/auth/routes.go
+++ internal/auth/routes.go
@@ -3,20 +3,14 @@ package auth
 import (
 	"net/http"
 
+	"cognito-repeater-go/internal/auth/deps"
 	"cognito-repeater-go/internal/auth/login"
 	"cognito-repeater-go/internal/auth/logout"
 	"cognito-repeater-go/internal/auth/logoutredirect"
-	"cognito-repeater-go/internal/config"
 )
 
-type AuthHandlers struct {
-	LoginURLProvider    login.LoginURLProvider
-	LogoutURLProvider   logout.LogoutURLProvider
-	MetadataURLProvider config.MetadataURLProvider
-}
-
-func RegisterAuthRoutes(mux *http.ServeMux, h AuthHandlers) {
-	mux.HandleFunc("/login", login.LoginHandler(h.LoginURLProvider, h.MetadataURLProvider))
-	mux.HandleFunc("/logout", logout.LogoutHandler(h.LogoutURLProvider, h.MetadataURLProvider))
+func RegisterAuthRoutes(mux *http.ServeMux, d deps.HandlerDependencies) {
+	mux.HandleFunc("/login", login.LoginHandler(d))
+	mux.HandleFunc("/logout", logout.LogoutHandler(d))
 	mux.HandleFunc("/logout/redirect", logoutredirect.LogoutRedirectHandler)
 }
