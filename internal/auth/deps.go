a290045499490b07e7afc7adcb3762376ead07a6
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-24 08:08:56 +0900

    chore(auth): create internal/auth/deps.go; add HandlerDependencies

---
+++ internal/auth/deps.go
@@ -0,0 +1,11 @@
+package auth
+
+import (
+	"cognito-repeater-go/internal/auth/config"
+	"cognito-repeater-go/internal/httpclient"
+)
+
+type HandlerDependencies struct {
+	Config     *config.Config
+	HTTPClient httpclient.HTTPClient
+}
