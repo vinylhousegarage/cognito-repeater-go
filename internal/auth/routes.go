1dff71d127fec724691871db765685e69ef45f9c
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-24 17:00:21 +0900

    feat(router): register /callback route

--- internal/auth/routes.go
+++ internal/auth/routes.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"net/http"
 
+	"cognito-repeater-go/internal/auth/callback"
 	"cognito-repeater-go/internal/auth/deps"
 	"cognito-repeater-go/internal/auth/login"
 	"cognito-repeater-go/internal/auth/logout"
@@ -10,6 +11,7 @@ import (
 )
 
 func RegisterAuthRoutes(mux *http.ServeMux, d deps.HandlerDependencies) {
+	mux.HandleFunc("/callback", callback.CallbackHandler(d))
 	mux.HandleFunc("/login", login.LoginHandler(d))
 	mux.HandleFunc("/logout", logout.LogoutHandler(d))
 	mux.HandleFunc("/logout/redirect", logoutredirect.LogoutRedirectHandler)
