117b98abeef49eed1db8af0ecb120c7ebfb647ba
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-24 12:22:52 +0900

    chore(test): remove underscores from test function name

--- test/auth/logout_redirect_route_test.go
+++ test/auth/logout_redirect_route_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRouter_LogoutRedirectRoute_ReturnsExpectedJSON(t *testing.T) {
+func TestRouterLogoutRedirectRouteReturnsExpectedJSON(t *testing.T) {
 	t.Parallel()
 
 	handlerDeps := deps.HandlerDependencies{
