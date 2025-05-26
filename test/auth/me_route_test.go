7f4cf45dc767684c30c593b07262318e0c4db4e1
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-26 21:37:51 +0900

    fix(test): mock and use jwks

--- test/auth/me_route_test.go
+++ test/auth/me_route_test.go
@@ -36,16 +36,22 @@ func TestMeHandler_Integration_Success(t *testing.T) {
 
 			case strings.Contains(req.URL.String(), "/jwks"):
 				rec.WriteHeader(http.StatusOK)
-				if _, err := rec.WriteString(`{
-					"keys": [{
-						"kid": "example-kid",
-						"kty": "RSA",
-						"alg": "RS256",
-						"use": "sig",
-						"n": "vYy1yk3nLfNDFY-9XYMGhW-m1q5vJiwxDsPeHcYRhG3JuL2Rkxn5HDZ_Kr-WD1CTGjhdOBhnGlm9M7tRkhWbyU8c_OvY7nXU2PlNWdI3RuXb9hxCD6yXv_T7pHeKjdO6uT0vZ0DOZyMG8OdSfaCtb-Kdlh6UMtURO--OE1sMjj3SuBlRm6TYEkAM7Gv36k8b5kDp2uDtj_l_WnDZf81-fCVPvHfU1UmgRRfL_UWT1ZLkZADKHZlSgOXrPK2aD4eQmTIpNj03kpct8rzoC0XyCqKPyO8W3PoYk6ytAPk7NV8og8JTx17C3_Mqz02GZud7FneX9XZ8ReXBfBqAfXYL9UQ",
-						"e": "AQAB"
-					}]
-				}`); err != nil {
+
+				jwks, _, err := testhelpers.GenerateTestJWKS()
+				if err != nil {
+					log.Printf("failed to generate test jwks: %v", err)
+					rec.WriteHeader(http.StatusInternalServerError)
+					return rec.Result(), nil
+				}
+
+				data, err := json.Marshal(jwks)
+				if err != nil {
+					log.Printf("failed to marshal test jwks: %v", err)
+					rec.WriteHeader(http.StatusInternalServerError)
+					return rec.Result(), nil
+				}
+
+				if _, err := rec.Write(data); err != nil {
 					log.Printf("failed to write mock jwks response: %v", err)
 				}
 
