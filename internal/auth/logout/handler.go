a4a0ab49dc646a26068ce0a8d691a5b0a6ccd471
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-29 16:42:55 +0900

    chore(logout): switch GetLogoutURL arguments order

--- internal/auth/logout/handler.go
+++ internal/auth/logout/handler.go
@@ -17,7 +17,7 @@ type LogoutEndpointProvider interface {
 func LogoutHandler(p LogoutEndpointProvider, c httpclient.HTTPClient) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		metadataURL := p.MetadataURL()
-		endpoint, err := GetLogoutURL(c, metadataURL)
+		endpoint, err := GetLogoutURL(metadataURL, c)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
