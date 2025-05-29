4cee595ea99ddbba32b71ac67abbf649cd6a7494
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-29 19:27:03 +0900

    chore(router): import internal/auth/callback

--- internal/router/router.go
+++ internal/router/router.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"cognito-repeater-go/internal/auth"
+	"cognito-repeater-go/internal/auth/callback"
 	"cognito-repeater-go/internal/auth/deps"
 	"cognito-repeater-go/internal/auth/logout"
 	"cognito-repeater-go/internal/config"
