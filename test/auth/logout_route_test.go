8a809bb457c426a7ea882ef36d1240ebe3cba259
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-29 19:34:09 +0900

    chore(test): add callback argument to NewRouter

--- test/auth/logout_route_test.go
+++ test/auth/logout_route_test.go
@@ -37,7 +37,7 @@ func TestLogoutRouteIsRegisteredInProductionRouter(t *testing.T) {
 		},
 	}
 
-	r := router.NewRouter(&MockLogoutConfig{}, &MockLogoutConfig{}, mockClient)
+	r := router.NewRouter(&MockLogoutConfig{}, &MockLogoutConfig{}, &MockLogoutConfig{}, mockClient)
 
 	req := httptest.NewRequest(http.MethodGet, "/logout", nil)
 	w := httptest.NewRecorder()
