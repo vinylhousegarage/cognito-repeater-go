9634113cd074a4966637af630b1ed38a0b163bde
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-30 08:41:45 +0900

    chore(login): use LoginHandlerProvider and httpclient.HTTPClient

--- internal/auth/login/handler.go
+++ internal/auth/login/handler.go
@@ -4,21 +4,22 @@ import (
 	"net/http"
 
 	"cognito-repeater-go/internal/auth/deps"
+	"cognito-repeater-go/internal/httpclient"
 )
 
-func LoginHandler(d deps.HandlerDependencies) http.HandlerFunc {
+func LoginHandler(p LoginHandlerProvider, c httpclient.HTTPClient) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		state := GenerateState()
 		http.SetCookie(w, BuildStateCookie(state))
 
-		metadataURL := d.Config.MetadataURL()
-		endpoint, err := GetLoginURL(metadataURL, d.HTTPClient)
+		metadataURL := p.MetadataURL()
+		endpoint, err := GetLoginURL(metadataURL, c)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		url, err := BuildLoginURL(d.Config, endpoint, state)
+		url, err := BuildLoginURL(p, endpoint, state)
 		if err != nil {
 			http.Error(w, "invalid login URL", http.StatusInternalServerError)
 			return
