ed1c4bd6a95c8fadec3b0a7e9a210d2a38021ac5
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-30 14:04:55 +0900

    chore(server): use RouterDependencies

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,14 +3,14 @@ package server
 import (
 	"net/http"
 
-	"cognito-repeater-go/internal/config"
+	"cognito-repeater-go/internal/auth"
 	"cognito-repeater-go/internal/httpclient"
 	"cognito-repeater-go/internal/router"
 )
 
-func NewServer(cfg *config.Config, client httpclient.HTTPClient) *http.Server {
+func NewServer(deps auth.RouteDependencies, cli httpclient.HTTPClient) *http.Server {
 	return &http.Server{
 		Addr:    ":8080",
-		Handler: router.NewRouter(cfg, cfg, cfg, cfg, cfg, client),
+		Handler: router.NewRouter(deps, cli),
 	}
 }
