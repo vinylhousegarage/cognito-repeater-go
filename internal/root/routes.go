f034c465e8377415dcc45f499ab38428da6080f4
vinylhousegarage <vinylhousegarage@gmail.com>
2025-06-03 09:53:32 +0900

    chore(root): remove NewPingHandler from RegisterRootRoutes

--- internal/root/routes.go
+++ internal/root/routes.go
@@ -3,11 +3,9 @@ package root
 import (
 	"net/http"
 
-	"cognito-repeater-go/internal/root/ping"
 	"cognito-repeater-go/internal/root/rootroot"
 )
 
 func RegisterRootRoutes(mux *http.ServeMux) {
-	mux.HandleFunc("/ping", ping.NewPingHandler)
 	mux.HandleFunc("/", rootroot.NewRootHandler)
 }
