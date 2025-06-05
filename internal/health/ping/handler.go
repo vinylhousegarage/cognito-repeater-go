1cac8334a212b9a1d53043ae9e3851e1f224c5a2
vinylhousegarage <vinylhousegarage@gmail.com>
2025-06-05 19:26:42 +0900

    chore(ping): use PingResponse struct and return JSON

--- internal/health/ping/handler.go
+++ internal/health/ping/handler.go
@@ -1,20 +1,23 @@
 package ping
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
 // @Summary Health check endpoint
 // @Description Returns "pong" to indicate the server is alive
 // @Tags health
-// @Produce plain
-// @Success 200 {string} string "pong"
+// @Produce json
+// @Success 200 {object} ping.PingResponse "pong message"
 // @Router /ping [get]
 func NewPingHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	if _, err := fmt.Fprint(w, "pong"); err != nil {
-		fmt.Printf("failed to write response: %v\n", err)
+
+	resp := PingResponse{Message: "pong"}
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		log.Printf("failed to write ping response: %v", err)
 	}
 }
