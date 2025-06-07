8d838a68983934eacd39c2c955b1bc9da8219dff
vinylhousegarage <vinylhousegarage@gmail.com>
2025-06-07 11:20:52 +0900

    chore(simulatederrors): import zap to routes.go; add logger argument to RegisterErrorRoutes and HandleFunc

--- internal/simulatederrors/routes.go
+++ internal/simulatederrors/routes.go
@@ -4,8 +4,10 @@ import (
 	"net/http"
 
 	"cognito-repeater-go/internal/simulatederrors/notfound"
+
+	"go.uber.org/zap"
 )
 
-func RegisterErrorRoutes(mux *http.ServeMux) {
-	mux.HandleFunc("/error/404", notfound.NewError404Handler)
+func RegisterErrorRoutes(mux *http.ServeMux, logger *zap.Logger) {
+	mux.HandleFunc("/error/404", notfound.NewError404Handler(logger))
 }
