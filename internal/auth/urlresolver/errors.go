8553f76e36073c3fc1d89f2e17cff14b017e6919
vinylhousegarage <vinylhousegarage@gmail.com>
2025-06-08 22:10:21 +0900

    chore(urlresolver): apply goimports to fix lint error

--- internal/auth/urlresolver/errors.go
+++ internal/auth/urlresolver/errors.go
@@ -3,7 +3,7 @@ package urlresolver
 import "errors"
 
 var (
-	ErrFailedToCreateRequest     = errors.New("failed to create request")
-	ErrFailedToDecodeMetadata    = errors.New("failed to decode metadata")
-	ErrFailedToFetchMetadata     = errors.New("failed to fetch metadata")
+	ErrFailedToCreateRequest  = errors.New("failed to create request")
+	ErrFailedToDecodeMetadata = errors.New("failed to decode metadata")
+	ErrFailedToFetchMetadata  = errors.New("failed to fetch metadata")
 )
