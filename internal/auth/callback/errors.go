41b82ddaea588ad4cfc7fe6e2bbcf6272c2db3c5
vinylhousegarage <vinylhousegarage@gmail.com>
2025-06-08 12:02:52 +0900

    chore(callback): define errors for GetCallbackURL in errors.go

--- internal/auth/callback/errors.go
+++ internal/auth/callback/errors.go
@@ -3,9 +3,14 @@ package callback
 import "errors"
 
 var (
-	ErrMissingCode        = errors.New("missing code")
-	ErrMissingState       = errors.New("missing state")
-	ErrMissingStateCookie = errors.New("missing oauth_state cookie")
-	ErrInvalidState       = errors.New("invalid state")
-	ErrEmptyTokenEndpoint = errors.New("token_endpoint is empty")
+	ErrEmptyTokenEndpoint        = errors.New("token_endpoint is empty")
+	ErrFailedToCreateRequest     = errors.New("failed to create request")
+	ErrFailedToDecodeMetadata    = errors.New("failed to decode metadata")
+	ErrFailedToFetchMetadata     = errors.New("failed to fetch metadata")
+	ErrInvalidMetadataNoEndpoint = errors.New("invalid metadata: token_endpoint is empty")
+	ErrInvalidState              = errors.New("invalid state")
+	ErrMissingCode               = errors.New("missing code")
+	ErrMissingState              = errors.New("missing state")
+	ErrMissingStateCookie        = errors.New("missing oauth_state cookie")
+	ErrUnexpectedStatusCode      = errors.New("unexpected status code from metadata endpoint")
 )
