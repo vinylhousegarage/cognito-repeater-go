9e90a4f3f025f2b3806908810e3b8b21c8b66350
vinylhousegarage <vinylhousegarage@gmail.com>
2025-06-09 11:48:51 +0900

    refactor(callback): add ErrInvalidMetadataNoEndpoint to errors.go

--- internal/auth/callback/errors.go
+++ internal/auth/callback/errors.go
@@ -4,6 +4,7 @@ import "errors"
 
 var (
 	ErrInvalidState              = errors.New("invalid state")
+	ErrInvalidMetadataNoEndpoint = errors.New("invalid metadata: token_endpoint is empty")
 	ErrMissingCode               = errors.New("missing code")
 	ErrMissingState              = errors.New("missing state")
 	ErrMissingStateCookie        = errors.New("missing oauth_state cookie")
