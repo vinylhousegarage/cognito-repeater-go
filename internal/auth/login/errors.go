cb18dee86b1d67228bf664dd48b728c7d311767b
vinylhousegarage <vinylhousegarage@gmail.com>
2025-06-09 20:44:37 +0900

    refactor(login): add ErrFailedToParseLoginURL to errors.go

--- internal/auth/login/errors.go
+++ internal/auth/login/errors.go
@@ -6,6 +6,7 @@ var (
 	ErrFailedToCreateRequest        = errors.New("failed to create request")
 	ErrFailedToDecodeMetadata       = errors.New("failed to decode metadata")
 	ErrFailedToFetchMetadata        = errors.New("failed to fetch metadata")
+	ErrFailedToParseLoginURL        = errors.New("failed to parse login endpoint URL")
 	ErrMissingAuthorizationEndpoint = errors.New("missing authorization_endpoint in metadata response")
 	ErrUnexpectedStatusCode         = errors.New("unexpected status code from metadata endpoint")
 )
