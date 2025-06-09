3ab60b8ce36c20e67761437fe30befa5ce0f375a
vinylhousegarage <vinylhousegarage@gmail.com>
2025-06-09 14:40:41 +0900

    refactor(callback): remove ErrEmptyTokenEndpoint from errors.go

--- internal/auth/callback/errors.go
+++ internal/auth/callback/errors.go
@@ -3,7 +3,6 @@ package callback
 import "errors"
 
 var (
-	ErrEmptyTokenEndpoint        = errors.New("token_endpoint is empty")
 	ErrFailedToCreateRequest     = errors.New("failed to create request")
 	ErrFailedToDecodeMetadata    = errors.New("failed to decode metadata")
 	ErrFailedToFetchMetadata     = errors.New("failed to fetch metadata")
