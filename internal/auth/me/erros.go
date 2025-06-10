95bc8251b89d021df4ccd40c4e5da5538820bd77
vinylhousegarage <vinylhousegarage@gmail.com>
2025-06-10 13:57:25 +0900

    refactor(me): define error constants for FetchJWKSet

--- internal/auth/me/erros.go
+++ internal/auth/me/erros.go
@@ -3,10 +3,13 @@ package me
 import "errors"
 
 var (
-	ErrFailedToCreateRequest  = errors.New("failed to create request")
-	ErrFailedToDecodeMetadata = errors.New("failed to decode metadata")
-	ErrFailedToFetchMetadata  = errors.New("failed to fetch metadata")
-	ErrFailedToParseLoginURL  = errors.New("failed to parse login endpoint URL")
-	ErrMissingJWKSURI         = errors.New("missing jwks_uri in metadata response")
-	ErrUnexpectedStatusCode   = errors.New("unexpected status code from metadata endpoint")
+	ErrFailedToCreateJWKSRequest = errors.New("failed to create jwks request")
+	ErrFailedToCreateRequest     = errors.New("failed to create request")
+	ErrFailedToDecodeJWKS        = errors.New("failed to decode jwks JSON")
+	ErrFailedToDecodeMetadata    = errors.New("failed to decode metadata")
+	ErrFailedToFetchJWKS         = errors.New("failed to fetch jwks")
+	ErrFailedToFetchMetadata     = errors.New("failed to fetch metadata")
+	ErrFailedToParseLoginURL     = errors.New("failed to parse login endpoint URL")
+	ErrMissingJWKSURI            = errors.New("missing jwks_uri in metadata response")
+	ErrUnexpectedStatusCode      = errors.New("unexpected status code from metadata endpoint")
 )
