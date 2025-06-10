6f0437b1e0608aa93a71b682120b58cf1e4c1311
vinylhousegarage <vinylhousegarage@gmail.com>
2025-06-11 08:53:06 +0900

    test(me): move generateTestToken from handler_test.go

--- internal/auth/me/handler_test.go
+++ internal/auth/me/handler_test.go
@@ -19,14 +19,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func generateTestToken(t *testing.T, claims jwt.RegisteredClaims, privateKey *rsa.PrivateKey) string {
-	t.Helper()
-	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
-	signed, err := token.SignedString(privateKey)
-	assert.NoError(t, err)
-	return signed
-}
-
 type MockMeHandlerProvider struct {
 	mockAudience    string
 	mockGetJWKSURI  string
