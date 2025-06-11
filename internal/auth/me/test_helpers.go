0201f0a6d799a96d4d93b68da7d9c9335ff822c1
vinylhousegarage <vinylhousegarage@gmail.com>
2025-06-11 10:32:19 +0900

    test(me): move generateTestToken from test_helpers.go

---
+++ internal/auth/me/test_helpers.go
@@ -0,0 +1,21 @@
+package me
+
+import (
+	"crypto/rsa"
+	"testing"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/stretchr/testify/assert"
+)
+
+func generateTestToken(t *testing.T, claims jwt.RegisteredClaims, privateKey *rsa.PrivateKey) string {
+	t.Helper()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+	signed, err := token.SignedString(privateKey)
+	assert.NoError(t, err)
+	return signed
+}
+
+var testTime = time.Date(2025, time.June, 10, 10, 0, 0, 0, time.UTC)
