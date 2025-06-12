9b7fea6f43bf2d5a0762be8b838ecf0ab20d0bc8
vinylhousegarage <vinylhousegarage@gmail.com>
2025-06-12 11:04:04 +0900

    refactor(me): add HTTP status code comments to error constants for clarity

--- internal/auth/utils/errors.go
+++ internal/auth/utils/errors.go
@@ -3,6 +3,6 @@ package utils
 import "errors"
 
 var (
-	ErrFailedToParseForm = errors.New("failed to parse form")
-	ErrMissingToken      = errors.New("token is missing from body")
+	ErrFailedToParseForm = errors.New("failed to parse form") // 400 Bad Request
+	ErrMissingToken      = errors.New("token is missing from body") // 400 Bad Request
 )
