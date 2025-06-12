80be7ebb989e1aa79932ae26839ec5da1d96a94b
vinylhousegarage <vinylhousegarage@gmail.com>
2025-06-12 12:11:09 +0900

    chore(me): apply goimports to fix lint error

--- internal/auth/utils/errors.go
+++ internal/auth/utils/errors.go
@@ -3,6 +3,6 @@ package utils
 import "errors"
 
 var (
-	ErrFailedToParseForm = errors.New("failed to parse form") // 400 Bad Request
+	ErrFailedToParseForm = errors.New("failed to parse form")       // 400 Bad Request
 	ErrMissingToken      = errors.New("token is missing from body") // 400 Bad Request
 )
