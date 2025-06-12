4376404260889dc7c14f9229cbc42aa49283b8e0
vinylhousegarage <vinylhousegarage@gmail.com>
2025-06-12 21:07:32 +0900

    fix(revoke): correct import path from internal/testhelpers to test/testhelpers in handler_test.go

--- internal/auth/revoke/handler_test.go
+++ internal/auth/revoke/handler_test.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"testing"
 
-	"cognito-repeater-go/internal/testhelpers"
+	"cognito-repeater-go/test/testhelpers"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
