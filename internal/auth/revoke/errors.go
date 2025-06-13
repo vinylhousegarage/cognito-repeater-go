7ae9a599143919734d1e33b343bb2c29702723ff
vinylhousegarage <vinylhousegarage@gmail.com>
2025-06-13 22:17:31 +0900

    refactor(revoke): centralize error handling using WriteErrorResponse and define structured AppErrors

--- internal/auth/revoke/errors.go
+++ internal/auth/revoke/errors.go
@@ -6,12 +6,13 @@ import (
 )
 
 var (
-	ErrFailedToCreateRequest       = apperror.New(http.StatusInternalServerError, "failed to create request")        // 500 Internal Server Error
-	ErrFailedToCreateRevokeRequest = apperror.New(http.StatusInternalServerError, "failed to create revoke request") // 500 Internal Server Error
-	ErrFailedToDecodeMetadata      = apperror.New(http.StatusBadGateway, "failed to decode metadata")                // 502 Bad Gateway
-	ErrFailedToFetchMetadata       = apperror.New(http.StatusBadGateway, "failed to fetch metadata")                 // 502 Bad Gateway
-	ErrFailedToSendRevokeRequest   = apperror.New(http.StatusBadGateway, "failed to send revoke request")            // 502 Bad Gateway
-	ErrMissingClientCredentials    = apperror.New(http.StatusBadRequest, "missing client credentials")               // 400 Bad Request
-	ErrMissingRevocationEndpoint   = apperror.New(http.StatusBadGateway, "missing revocation_endpoint")              // 502 Bad Gateway
-	ErrUnexpectedStatusCode        = apperror.New(http.StatusBadGateway, "unexpected response from metadata")        // 502 Bad Gateway
+	ErrFailedToCreateRequest        = apperror.New(http.StatusInternalServerError, "failed to create request")        // 500 Internal Server Error
+	ErrFailedToCreateRevokeRequest  = apperror.New(http.StatusInternalServerError, "failed to create revoke request") // 500 Internal Server Error
+	ErrFailedToDecodeMetadata       = apperror.New(http.StatusBadGateway, "failed to decode metadata")                // 502 Bad Gateway
+	ErrFailedToFetchMetadata        = apperror.New(http.StatusBadGateway, "failed to fetch metadata")                 // 502 Bad Gateway
+	ErrFailedToSendRevokeRequest    = apperror.New(http.StatusBadGateway, "failed to send revoke request")            // 502 Bad Gateway
+	ErrMissingClientCredentials     = apperror.New(http.StatusBadRequest, "missing client credentials")               // 400 Bad Request
+	ErrMissingRevocationEndpoint    = apperror.New(http.StatusBadGateway, "missing revocation_endpoint")              // 502 Bad Gateway
+	ErrUnexpectedRevokeStatusCode   = apperror.New(http.StatusBadGateway, "unexpected response from revoke")          // 502 Bad Gateway
+	ErrUnexpectedMetadataStatusCode = apperror.New(http.StatusBadGateway, "unexpected response from metadata")        // 502 Bad Gateway
 )
