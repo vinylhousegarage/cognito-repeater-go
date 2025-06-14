21ddc3ae2020fcffcabd6ccc0651dd48fe4c4207
vinylhousegarage <vinylhousegarage@gmail.com>
2025-06-14 14:23:13 +0900

    fix(callback): rename incorrect ErrUnexpectedUserinfoStatusCode to ErrUnexpectedMetadataStatusCode

--- internal/auth/callback/errors.go
+++ internal/auth/callback/errors.go
@@ -14,5 +14,5 @@ var (
 	ErrMissingState                 = apperror.New(http.StatusBadRequest, "missing state")                     // 400 Bad Request
 	ErrMissingStateCookie           = apperror.New(http.StatusBadRequest, "missing oauth_state cookie")        // 400 Bad Request
 	ErrMissingTokenEndpoint         = apperror.New(http.StatusBadGateway, "missing token_endpoint")            // 502 Bad Gateway
-	ErrUnexpectedUserinfoStatusCode = apperror.New(http.StatusBadGateway, "unexpected response from userinfo") // 502 Bad Gateway
+	ErrUnexpectedMetadataStatusCode = apperror.New(http.StatusBadGateway, "unexpected response from userinfo") // 502 Bad Gateway
 )
