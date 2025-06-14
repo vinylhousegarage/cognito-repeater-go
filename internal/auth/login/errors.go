86b04c38470b47645d58380da84b8f03f53cf0ba
vinylhousegarage <vinylhousegarage@gmail.com>
2025-06-14 12:49:58 +0900

    refactor(login): add ErrFailedToParseLoginURL to errors.go

--- internal/auth/login/errors.go
+++ internal/auth/login/errors.go
@@ -6,9 +6,10 @@ import (
 )
 
 var (
-	ErrFailedToCreateRequest        = apperror.New(http.StatusInternalServerError, "failed to create request") // 500 Internal Server Error
-	ErrFailedToDecodeMetadata       = apperror.New(http.StatusBadGateway, "failed to decode metadata")         // 502 Bad Gateway
-	ErrFailedToFetchMetadata        = apperror.New(http.StatusBadGateway, "failed to fetch metadata")          // 502 Bad Gateway
-	ErrMissingAuthorizationEndpoint = apperror.New(http.StatusBadGateway, "missing authorization_endpoint")    // 502 Bad Gateway
-	ErrUnexpectedMetadataStatusCode = apperror.New(http.StatusBadGateway, "unexpected response from metadata") // 502 Bad Gateway
+	ErrFailedToCreateRequest        = apperror.New(http.StatusInternalServerError, "failed to create request")           // 500 Internal Server Error
+	ErrFailedToDecodeMetadata       = apperror.New(http.StatusBadGateway, "failed to decode metadata")                   // 502 Bad Gateway
+	ErrFailedToFetchMetadata        = apperror.New(http.StatusBadGateway, "failed to fetch metadata")                    // 502 Bad Gateway
+	ErrFailedToParseLoginURL        = apperror.New(http.StatusInternalServerError, "failed to parse login endpoint URL") // 500 Internal Server Error
+	ErrMissingAuthorizationEndpoint = apperror.New(http.StatusBadGateway, "missing authorization_endpoint")              // 502 Bad Gateway
+	ErrUnexpectedMetadataStatusCode = apperror.New(http.StatusBadGateway, "unexpected response from metadata")           // 502 Bad Gateway
 )
