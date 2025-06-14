ed4b642d31fa43585b6476692309c10759a8b757
vinylhousegarage <vinylhousegarage@gmail.com>
2025-06-14 15:11:29 +0900

    fix(callback): correct ErrUnexpectedMetadataStatusCode message to indicate metadata response error

--- internal/auth/callback/errors.go
+++ internal/auth/callback/errors.go
@@ -15,5 +15,5 @@ var (
 	ErrMissingStateCookie           = apperror.New(http.StatusBadRequest, "missing oauth_state cookie")        // 400 Bad Request
 	ErrMissingTokenEndpoint         = apperror.New(http.StatusBadGateway, "missing token_endpoint")            // 502 Bad Gateway
 	ErrUnexpectedCallbackStatusCode = apperror.New(http.StatusBadGateway, "unexpected response from callback") // 502 Bad Gateway
-	ErrUnexpectedMetadataStatusCode = apperror.New(http.StatusBadGateway, "unexpected response from userinfo") // 502 Bad Gateway
+	ErrUnexpectedMetadataStatusCode = apperror.New(http.StatusBadGateway, "unexpected response from metadata") // 502 Bad Gateway
 )
