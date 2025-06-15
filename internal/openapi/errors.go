c16e34ffbad5abf9cce4b2d737cfbb7e7a1efaa0
vinylhousegarage <vinylhousegarage@gmail.com>
2025-06-16 00:25:22 +0900

    refactor(openapi): remove ErrFailedToReadOpenAPISpec

--- internal/openapi/errors.go
+++ internal/openapi/errors.go
@@ -9,6 +9,5 @@ import (
 var (
 	ErrFailedToLoadOpenAPISpec     = apperror.New(http.StatusInternalServerError, "Failed to load OpenAPI spec")     // 500 Internal Server Error
 	ErrFailedToMarshalOpenAPISpec  = apperror.New(http.StatusInternalServerError, "failed to marshal OpenAPI spec")  // 500 Internal Server Error
-	ErrFailedToReadOpenAPISpec     = apperror.New(http.StatusInternalServerError, "Failed to read OpenAPI spec")     // 500 Internal Server Error
 	ErrFailedToValidateOpenAPISpec = apperror.New(http.StatusInternalServerError, "Failed to validate OpenAPI spec") // 500 Internal Server Error
 )
