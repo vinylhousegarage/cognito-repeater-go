6dce9ba39cd7805e3002622fbdfea70558487fe4
vinylhousegarage <vinylhousegarage@gmail.com>
2025-06-22 15:16:12 +0900

    feat(ping): add panic case to ping handler

--- internal/health/ping/response.go
+++ internal/health/ping/response.go
@@ -2,6 +2,6 @@ package ping
 
 // PingResponse represents a simple health check response.
 // Typically used for /ping endpoint.
-type PingResponse struct {
-	Message string `json:"message" example:"pong"`
-}
+// type PingResponse struct {
+// 	Message string `json:"message" example:"pong"`
+// }
